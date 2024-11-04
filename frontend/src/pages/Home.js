f4d39e972faff2ae487ce21a25b8e560b5b92477
Tuba Hussain <hussaintuba12345@gmail.com>
2024-11-04 18:29:32 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = 'https://authentication-app-api-one.vercel.app//products';
+            const url = 'https://authentication-app-api-one.vercel.app/products';
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
